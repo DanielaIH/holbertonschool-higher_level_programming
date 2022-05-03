e740516e744a40a2065d7333fd48a479796eaa9b
DanielaIH <4427@holbertonschool.com>
2022-05-02 19:54:57 -0700

    ADD lists.h 10-check_cycle.c a function in C that checks if a singly linked list has a cycle in it.

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check the code
+ * @list: pointer to first element of linked list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current;
+
+	current = list;
+	current = current->next;
+	while (current != NULL)
+	{
+		if (current == list)
+			return (1);
+		current = current->next;
+	}
+	return (0);
+}
