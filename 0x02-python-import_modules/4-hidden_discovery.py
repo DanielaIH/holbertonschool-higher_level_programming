75fd58a1ca961c279a65e6f5eb7488355e63c7ed
DanielaIH <4427@holbertonschool.com>
2022-05-05 22:07:01 -0500

    FIX sort 4-hidden_discovery.py program that prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 import hidden_4
 if __name__ == '__main__':
     f_name = dir(hidden_4)
-    #f_name.sort()
+    f_name.sort()
     for x in f_name:
         if "__" not in x:
-            print(x)
+            print(f"{x}")
