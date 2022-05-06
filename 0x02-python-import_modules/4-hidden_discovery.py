3fe98655f6e08cdddcbebd4bbddc35c3ccc4f5c6
DanielaIH <4427@holbertonschool.com>
2022-05-05 22:12:36 -0500

    FIX undefine variables 4-hidden_discovery.py program that prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import hidden_4
-    f_name = dir(hidden_4)
-    f_name.sort()
-    for x in f_name:
+    for x in dir(hidden_4).sort():
         if "__" not in x:
-            print(f"{x}")
+            print(x)
