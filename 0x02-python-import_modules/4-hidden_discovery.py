e1eac5147d8dba55c82725d3427483b053694469
DanielaIH <4427@holbertonschool.com>
2022-05-05 22:05:05 -0500

    CHANGE hidden  4-hidden_discovery.py program that prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
-import hidden_4 as hd
+#!/usr/bin/python3
+import hidden_4
 if __name__ == '__main__':
-    f_name = dir(hd)
-    f_name.sort()
+    f_name = dir(hidden_4)
+    #f_name.sort()
     for x in f_name:
         if "__" not in x:
             print(x)
