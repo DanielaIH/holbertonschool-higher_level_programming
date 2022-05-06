fc24eefae555224bcd8100999ddbed2f0796618d
DanielaIH <4427@holbertonschool.com>
2022-05-05 22:31:19 -0500

    FIX remove last space in 4-hidden_discovery.py program that prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+import hidden_4
 if __name__ == '__main__':
-    import hidden_4
-    for x in dir(hidden_4).sort():
-        if x[0] != '_':
+    for x in dir(hidden_4):
+        if "__" not in x:
             print(x)
