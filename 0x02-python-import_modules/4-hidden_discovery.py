7587b77bd882563f001bfbf9dafc90f5fc51c975
DanielaIH <4427@holbertonschool.com>
2022-05-05 21:56:17 -0500

    ADD 4-hidden_discovery.py program that prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import hidden_4 as hd
-f_name = dir(hd)
-f_name.sort()
-for x in f_name:
-    if "__" not in x:
-        print(x)
+if __name__ == '__main__':
+    f_name = dir(hd)
+    f_name.sort()
+    for x in f_name:
+        if "__" not in x:
+            print(x)
