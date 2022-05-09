8f25bfe6e7211813012c58fa73b0a00fc5eab030
DanielaIH <4427@holbertonschool.com>
2022-05-09 14:49:48 -0700

    FIX change reverse by reversed range 3-print_reversed_list_integer.py function that prints all integers of a list, in reverse order.

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if not my_list:
-        pass
-    my_list.reverse()
-    for x in range(len(my_list)):
-        print(my_list[x])
+    for x in reversed(my_list):
+        print(x)
