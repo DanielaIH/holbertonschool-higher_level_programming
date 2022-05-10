fd69acfbf6e5fd65dc7ebfe5fa01e9ff1fcbf836
DanielaIH <4427@holbertonschool.com>
2022-05-09 18:01:28 -0700

    FIX 3-print_reversed_list_integer.py IF ELSE sentence

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     if not my_list:
         pass
-    for x in reversed(my_list):
-        print(x)
+    else:
+        for x in reversed(my_list):
+            print(x)
