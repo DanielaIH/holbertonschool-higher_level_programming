ec9fcf98fc428d702fe162f1a88c702ae7beb5d4
DanielaIH <4427@holbertonschool.com>
2022-05-23 19:41:01 -0700

    UPDATE check for ValueError

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,7 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
             new_list.append(0)
             i += 1
-        except TypeError:
+        except (TypeError, ValueError):
             print("wrong type")
             new_list.append(0)
             i += 1
