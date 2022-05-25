653f89a1b1d63b3946f3a370e5f32bf17ead0aa9
DanielaIH <4427@holbertonschool.com>
2022-05-24 20:42:26 -0700

    ADD 4-square.py class Square that defines a square by: (based on 3-square.py)

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""class Square - a class that defines a square"""
+
+
+class Square:
+    """
+    class Square - a class that defines a square
+
+   __init__: creates a square object.
+   area: returns the current square area
+
+    Atributes:
+        __size (no type/value verification)
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """ returns the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        area method: returns the current square area
+
+        Returns:
+            the current square area
+        """
+        return (self.__size ** 2)
