91f54db766855f4171f99d33cf7620cec65e3b71
DanielaIH <4427@holbertonschool.com>
2022-05-31 19:01:26 -0700

    ADD 1-rectangle.py  class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Rectangle - defines a rectangle"""
+
+
+class Rectangle():
+
+    def __init__(self, width=0, height=0):
+        """The _init_ method defines the properties of Rectangle"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Return property width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set property width"""
+        if (type(value) is not int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Return property height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set property height"""
+        if (type(value) is not int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
