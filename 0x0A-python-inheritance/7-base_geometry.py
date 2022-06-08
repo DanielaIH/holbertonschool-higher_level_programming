6ee3adbc9d99c3ae63be319ce037c37030d601f1
DanielaIH <4427@holbertonschool.com>
2022-06-07 17:23:04 -0700

    ADD 7-base_geometry.py Public instance method: def integer_validator(self, name, value): that validates value

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Area - is an empty module"""
+
+
+class BaseGeometry():
+    """Area is not implemented"""
+    def area(self):
+        """Area is not implemented"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        self.name = name
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+        else:
+            self.value = value
