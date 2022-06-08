e5f6550cf8c2812fa663cbe300825aa5d6fe8713
DanielaIH <4427@holbertonschool.com>
2022-06-07 19:37:51 -0700

    ADD 9-rectangle.py

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""0x0A. Python - Inheritance"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle - is a DerivedClass"""
+    def __init__(self, width, height):
+        """__init__ - validates and inizializes width and height
+        @width: is the width of the Rectangle
+        @height: is the height of the Rectangle
+        """
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def __str__(self):
+        """__str__ - returns a rectangle description"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+
+    def area(self):
+        """area - method that returns the area"""
+        return (self.__width * self.__height)
