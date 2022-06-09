4fcc7e5e3a09e256b678983cd9c8dc4f971ced3c
DanielaIH <4427@holbertonschool.com>
2022-06-08 21:48:00 -0700

    ADD 3-to_json_string.py function that returns the JSON representation of an object (string)

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,6 +2,7 @@
 """0x0B-python-input_output - 3-to_json_string.py"""
 import json
 
+
 def to_json_string(my_obj):
     """to_json_string - JSON representation of an object"""
     return(json.JSONEncoder().encode(my_obj))
