424c696a479441b074205b9a19372f6beec05a4c
DanielaIH <4427@holbertonschool.com>
2022-06-08 21:40:28 -0700

    Documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
+    """read_file - read the file filename"""
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end="")
