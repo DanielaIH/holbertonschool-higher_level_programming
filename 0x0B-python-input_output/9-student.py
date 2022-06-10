cc84ee5ec7639f16ee969c1dd9fd266e8cc48ee9
DanielaIH <4427@holbertonschool.com>
2022-06-09 19:43:35 -0700

    ADD 9-student.py class Student that defines a student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,6 +6,7 @@ class_to_json = __import__('8-class_to_json').class_to_json
 class Student():
     """Student - defines a student"""
     def __init__(self, first_name, last_name, age):
+        """Inicialize attributes"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
