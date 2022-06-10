9c4d94244f959246c3f7482064317791c6776fa1
DanielaIH <4427@holbertonschool.com>
2022-06-09 18:32:36 -0700

    FIX return

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     """load_from_json_file - function that creates an Object"""
     with open(filename, 'r', encoding="utf-8") as f:
-        json.load(f)
+        return json.load(f)
