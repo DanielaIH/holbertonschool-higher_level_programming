a3b5a81e71da5cb18c8e9ac83a452c843986c56b
DanielaIH <4427@holbertonschool.com>
2022-06-09 19:50:17 -0700

    ADD 9-student.py class Student that defines a student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """0x0B-python-input_output - 9-student.py"""
-class_to_json = __import__('8-class_to_json').class_to_json
 
 
 class Student():
@@ -13,4 +12,4 @@ class Student():
 
     def to_json(self):
         """to_json - retrieves a dictionary representation"""
-        return class_to_json(self)
+        return self.__dict__
