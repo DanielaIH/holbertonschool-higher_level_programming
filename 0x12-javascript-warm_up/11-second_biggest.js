b6de4dd958ebba5ee79078ad29a72a028a32b7ae
DanielaIH <4427@holbertonschool.com>
2022-08-15 22:44:19 -0500

    UPDATE 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,9 @@ const args = process.argv;
 if (args.length < 4) {
   console.log(0);
 } else {
-  const arrayOfNumbers = args.map(Number);
-  arrayOfNumbers.sort().reverse();
-  console.log(arrayOfNumbers[3]);
+  const numbers = args.map(Number);
+  numbers.sort().reverse();
+  let i = 0;
+  while (isNaN(numbers[i])) { i++; }
+  console.log(numbers[i + 1]);
 }
