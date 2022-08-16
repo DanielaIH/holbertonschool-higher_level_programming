5a3ae320e47888b6620763ea995fd8717c0c27fc
DanielaIH <4427@holbertonschool.com>
2022-08-16 18:31:33 -0500

    ADD 6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+  charPrint(c) {
+    if (!c) {
+        c = 'X';
+    }
+    let counter = 0;
+    while (counter < this.height) {
+      console.log(c.repeat(this.width));
+      counter++;
+    }
+  }
+}
+
+module.exports = Square;
