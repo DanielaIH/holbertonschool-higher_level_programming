1936f6acdca6c4cdc74e55afedc5af0cf79a7278
DanielaIH <4427@holbertonschool.com>
2022-08-17 19:43:14 -0500

    ADD 0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv;
+const fs = require('fs');
+fs.readFile(args[2], 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+});
