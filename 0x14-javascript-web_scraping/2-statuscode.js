ab0094124c1c0702ea004ea9c9bc059a17cdf8dc
DanielaIH <4427@holbertonschool.com>
2022-08-17 20:07:26 -0500

    ADD 2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const axios = require('axios').default;
+const args = process.argv;
+axios.get(args[2])
+  .then(function (response) {
+    console.log('code: ' + response.status);
+  })
+  .catch(function () {
+    console.log('code: 404');
+  });
