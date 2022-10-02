39d0d28945bd7b322cc1eb43755470fcc3e66e76
DanielaIH <4427@holbertonschool.com>
2022-10-02 11:49:12 -0700

    ADD 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""script that takes in a URL, and sends a POST request to
+the passed URL with the email as a parameter"""
+
+from urllib import request, parse
+from sys import argv
+
+
+if __name__ == "__main__":
+    """main content"""
+    post_data = parse.urlencode({"email": argv[2]}).encode()
+
+    with request.urlopen(request.Request(argv[1])) as response:
+        print(response.read().decode('utf-8'))
