3192d3af0ce5082b093f904baba9781b1f85e54a
DanielaIH <4427@holbertonschool.com>
2022-10-02 18:35:17 -0700

    ADD 7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" script that takes in a URL, sends a request to the URL
+and displays the body of the response."""
+
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    """Main content"""
+    req = requests.get(argv[1])
+    if req.status_code >= 400:
+        print("Error code: ", req.status_code)
+    else:
+        print(req.text)
