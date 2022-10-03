0141d1b12cbd2411f78ff3e156a048831284766b
DanielaIH <4427@holbertonschool.com>
2022-10-02 17:52:22 -0700

    ADD 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,5 +5,7 @@ and displays the value of the variable X-Request-Id"""
 import requests
 from sys import argv
 
-req = requests.get(argv[1])
-print(req.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    """Main content"""
+    req = requests.get(argv[1])
+    print(req.headers.get('X-Request-Id'))
